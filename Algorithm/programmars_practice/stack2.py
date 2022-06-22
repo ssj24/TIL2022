dfc397e3743ec88a8128a1564ca5cf62c767262f
ssj24 <sujee0824@gmail.com>
2022-06-22 21:14:12 +0900

    Add algorithm code

--- Algorithm/programmars_practice/stack2.py
+++ Algorithm/programmars_practice/stack2.py
@@ -1,21 +1,37 @@
-from collections import deque
+# from collections import deque
+# def solution(priorities, location):
+#     answer = 0
+#     q = deque(priorities)
+#     while q:
+#         cur = q.popleft()
+#         location -= 1
+#         if not q: 
+#             answer += 1
+#             return answer
+#         if max(q) > cur:
+#             if location < 0: location = len(q)
+#             q.append(cur)
+#         else:
+#             answer += 1
+#             if location == -1:
+#                 return answer
+
+#     return answer
+
 def solution(priorities, location):
     answer = 0
-    q = deque(priorities)
-    while q:
-        cur = q.popleft()
-        location -= 1
-        if not q: 
-            answer += 1
-            return answer
-        if max(q) > cur:
-            if location < 0: location = len(q)
-            q.append(cur)
+    search, c = sorted(priorities, reverse=True), 0
+    while True:
+        for i, priority in enumerate(priorities):
+            s = search[c]
+            if priority == s:
+                c += 1
+                answer += 1
+                if i == location:
+                    break
         else:
-            answer += 1
-            if location == -1:
-                return answer
-
+            continue
+        break
     return answer
 
 pri1 = [2, 1, 3, 2]
